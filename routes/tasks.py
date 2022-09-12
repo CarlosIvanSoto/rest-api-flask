070f1b98af263842b748430042f2dc22426d3d03
Carlos Ivan Soto <113265409+CarlosIvanSoto@users.noreply.github.com>
2022-09-11 23:29:45 -0500

    update, delete, complet
    # NEW ROUTES ADD
    UPDATE TAST FOR ID
    DELETE TASK FOR ID
    COMPLET TASK FOR ID

--- routes/tasks.py
+++ routes/tasks.py
@@ -48,14 +48,14 @@ def getTasks():
 @app.route("/api/tasks/<string:name>", methods=["GET"])
 def getTask(name):
     try:
-        data = model.query.filter(model.name.like(f"%{name}%")).first()
-        if not data:
+        data = model.query.filter(model.name.like(f"%{name}%"))
+        if not data.first():
             return jsonify({
                 "count": 0,
                 "message":"task not exist",
                 "results": {}
             }), 200
-        toReturn = data.serialize()
+        toReturn = [one.serialize() for one in data]
         return jsonify({
             "count": len(toReturn),
             "message":"task's list",
@@ -65,3 +65,66 @@ def getTask(name):
         exception("[SERVER]: Error in route /api/tasks/<string:name> ->")
         return jsonify({"message": "Internal Error"}), 500
 #update task for id
+@app.route("/api/tasks/<int:id>", methods=["PUT"])
+def updateTask(id):
+    try:
+        data = model.query.get(id)
+        if not data:
+            return jsonify({
+                "count": 0,
+                "message":"task not exist",
+                "results": {}
+            }), 200
+        [setattr(data, key, request.json[key]) for key in request.json]            
+        db.session.commit()
+        toReturn = data.serialize()
+        return jsonify({
+            "count": 1,
+            "message":"task update Successfully",
+            "results": toReturn
+        }), 200
+    except Exception:
+        exception("[SERVER]: Error in route /api/tasks/<int:id> [PUT]->")
+        return jsonify({"message": "Internal Error"}), 500
+#Delete task from id
+@app.route("/api/tasks/<int:id>", methods=["DELETE"])
+def deleteTask(id):
+    try:
+        data = model.query.get(id)
+        if not data:
+            return jsonify({
+                "count": 0,
+                "message":"task not exist",
+                "results": {}
+            }), 200
+        db.session.delete(data)
+        db.session.commit()
+        return jsonify({
+            "message":"task delete Successfully",
+        }), 200
+    except Exception:
+        exception("[SERVER]: Error in route /api/tasks/<int:id> [DELETE] ->")
+        return jsonify({"message": "Internal Error"}), 500
+#complet task for id
+@app.route("/api/tasks/completed/<int:id>", methods=["PUT"])
+def completedTask(id):
+    try:
+        data = model.query.get(id)
+        if not data:
+            return jsonify({
+                "count": 0,
+                "message":"task not exist",
+                "results": {}
+            }), 200 
+        
+        data.completed = 0 if data.completed else 1
+        db.session.commit()
+        toReturn = data.serialize()
+        return jsonify({
+            "count": 1,
+            "message":"task completed Successfully",
+            "results": toReturn
+        }), 200
+    except Exception:
+        exception("[SERVER]: Error in route /api/tasks/completed/<int:id> [PUT]->")
+        return jsonify({"message": "Internal Error"}), 500
